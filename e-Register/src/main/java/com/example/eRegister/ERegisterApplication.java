c105e3fbc4359e08bc88d7bb451ce5fdfeadc77f
Sirak <sirak123hw@gmail.com>
2021-07-10 04:58:20 -0500

    Lab-10

---
+++ e-Register/src/main/java/com/example/eRegister/ERegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.eRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERegisterApplication.class, args);
+	}
+
+}
---
+++ lab10/JUnitTesting/src/main/java/edu/miu/cs/cs425/lab10/prob2_utils/IArrayFlattenerService.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs425.lab10.prob2_utils;
+
+public interface IArrayFlattenerService {
+
+    int[] flattenArray(int[][] input);
+
+}
